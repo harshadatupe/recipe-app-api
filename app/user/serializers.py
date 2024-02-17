499c0d89e1f60934a57b5d8e46232f480ae31a20
Harshada Tupe <harshadatupe8@gmail.com>
2024-02-16 18:05:07 -0800

    configured our first user api users/create

---
+++ app/user/serializers.py
@@ -0,0 +1,23 @@
+"""
+Serializers for the user API View.
+Serializers are the way to convert objects to and from Python objects,
+it takes json input may be from POST method of API, validates it and 
+converts it to model in our database or Python object.
+"""
+from django.contrib.auth import get_user_model, authenticate
+from django.utils.translation import ugettext_lazy as _
+
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """Serializer for the users object"""
+
+    class Meta:
+        model = get_user_model()
+        fields = ('email', 'password', 'name')
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
+
+    def create(self, validated_data):
+        """Create a new user with encrypted password and return it"""
+        return get_user_model().objects.create_user(**validated_data)
