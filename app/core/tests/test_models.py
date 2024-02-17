aedf21391b85e545d45f8f0c3ced2bc706001315
Harshada Tupe <harshadatupe8@gmail.com>
2024-02-17 11:32:35 -0800

    added recipe api model for tags

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -7,6 +7,11 @@ from django.contrib.auth import get_user_model
 from core import models
 
 
+def create_user(email='test@example.com', password='testpass123'):
+    """Create and return a new user."""
+    return get_user_model().objects.create_user(email, password)
+
+
 class ModelTests(TestCase):
     """Test models."""
 
@@ -63,3 +68,10 @@ class ModelTests(TestCase):
         )
 
         self.assertTrue(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        """Test creating a tag is successful."""
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name='Tag1')
+
+        self.assertEqual(str(tag), tag.name)
