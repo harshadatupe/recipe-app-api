2bf80225ade05f99caefbbe214f2b794a67c721f
Harshada Tupe <harshadatupe8@gmail.com>
2024-02-17 17:48:19 -0800

    added upload image rest endpoint and tests.

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -63,3 +63,14 @@ class RecipeDetailSerializer(RecipeSerializer):
         fields = RecipeSerializer.Meta.fields + ['description']
 
 
+class RecipeImageSerializer(serializers.ModelSerializer):
+    """Serializer for uploading images to recipes."""
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'image']
+        read_only_fields = ['id']
+        extra_kwargs = {'image': {'required': 'True'}}
+
+
+
